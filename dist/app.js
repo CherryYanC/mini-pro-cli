d7ad4833c5d4c1d96dbd3fbf094ce02de3a5c492
yancheng <luck_yan_0801@163.com>
2021-05-28 17:42:44 +0800

    fix: del node_modules and dist

--- dist/app.js
+++
@@ -1,2 +0,0 @@
-"use strict";App({globalData:{},onLaunch:function(){var o=wx.getStorageSync("logs")||[];o.unshift(Date.now()),wx.setStorageSync("logs",o),wx.login({success:function(o){console.log(o.code)}})}});
-//# sourceMappingURL=app.js.map
--- dist/pages/logs/logs.js
+++
@@ -1,2 +0,0 @@
-"use strict";Page({data:{logs:[]},onLoad:function(){this.setData({logs:(wx.getStorageSync("logs")||[]).map(function(t){return{date:new Date(t),timeStamp:t}})})}});
-//# sourceMappingURL=logs.js.map
